ffe5eb16fa089af729fac1f931984bddf79dc726
Muhammad-Rubel <rubel.contact@gmail.com>
2023-02-04 17:01:33 +0600

    Updates

---
+++ client/_app/immutable/chunks/2-00839ef8.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-efbd303c.js";export{t as component};
---
+++ server/nodes/2.js
@@ -0,0 +1,8 @@
+
+
+export const index = 2;
+export const component = async () => (await import('../entries/pages/_page.svelte.js')).default;
+export const file = '_app/immutable/components/pages/_page.svelte-efbd303c.js';
+export const imports = ["_app/immutable/components/pages/_page.svelte-efbd303c.js","_app/immutable/chunks/index-1664d19c.js"];
+export const stylesheets = [];
+export const fonts = [];
