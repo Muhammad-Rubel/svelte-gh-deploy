c0fd324d73f8fdad086eb8c31bb0d13eff130cbd
Muhammad-Rubel <rubel.contact@gmail.com>
2023-02-07 16:31:13 +0600

    added style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-5xl">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+	h1 {
+		color: red;
+	}
+
+	.text-5xl {
+		font-size: 3rem;
+	}
+</style>
